2b8730187321c8689292f9d03d2c8e6ef4215faf
dhruten <dhrutenpatel@gmail.com>
2020-01-07 15:04:36 +0530

    Added  Module for error logs

---
+++ errorutils/src/main/java/myservice/dhrutenpatel/example/com/errorutils/ErrorLogs.java
@@ -0,0 +1,10 @@
+package myservice.dhrutenpatel.example.com.errorutils;
+
+import android.util.Log;
+
+public class ErrorLogs {
+    public static void ErrorLog(String errorTag,String errorMessage)
+    {
+        Log.e(errorTag,"Error message from library  "+errorMessage);
+    }
+}
