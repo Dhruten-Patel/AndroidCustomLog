8a71f2d3dbb090ac30fbd65748c4d4e36ebd76f8
dhruten <dhrutenpatel@gmail.com>
2020-01-07 11:09:21 +0530

    create myLibrary

---
+++ mylib/src/main/java/myservice/dhrutenpatel/example/com/mylib/LogDetails.java
@@ -0,0 +1,9 @@
+package myservice.dhrutenpatel.example.com.mylib;
+
+import android.util.Log;
+
+public class LogDetails {
+    public static void printLog(String message){
+        Log.d("logDetails", " "+message);
+    }
+}
